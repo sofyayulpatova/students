3c2df8caad72e86caa19551ffa372168c4b87d62
sofya <dvkrrrr@gmail.com>
2022-03-11 10:41:20 +0200

    errors

--- app/routes.py
+++ app/routes.py
@@ -8,6 +8,18 @@ from app.forms import LoginForm
 from werkzeug.urls import url_parse
 
 
+# errors
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
+
+
 # greeting page
 @app.route('/')
 def index():
